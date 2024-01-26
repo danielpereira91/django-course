f80c72b67fa073e4fd2011ad9f080c5e93ef996a
Daniel Teixeira Pereira <97935464+danielpereira91@users.noreply.github.com>
2024-01-26 10:56:37 -0300

    'contato' and 'sobre' deleteds

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, template_name='recipes/home.html')
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
