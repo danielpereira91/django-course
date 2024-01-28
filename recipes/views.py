149f7606422c9418d0c43c9932c23301efb91627
Daniel Teixeira Pereira <97935464+danielpereira91@users.noreply.github.com>
2024-01-28 16:51:44 -0300

    changed path of home.html, including 'pages'

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, template_name='recipes/home.html')
+    return render(request, template_name='recipes/pages/home.html')
