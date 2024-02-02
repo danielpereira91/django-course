24c651772f9ee5ea92e9cd7a9244dfc8c963cd92
Daniel Teixeira Pereira <97935464+danielpereira91@users.noreply.github.com>
2024-02-02 13:36:37 -0300

    added recipe-view.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, template_name='recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
